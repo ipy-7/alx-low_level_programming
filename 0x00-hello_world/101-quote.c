782ddec4f1ec8ccd8dabb9e3bd15e659204d3ce4
ipy-7 <ipy-7@github.com>
2023-06-15 09:03:23 -0700

    W

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,8 @@
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	const char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, s);
 	return (1);
 }
