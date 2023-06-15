0f2f16149c39e286ea5181f99b92cbce57312b3e
ipy-7 <ipy-7@github.com>
2023-06-15 09:18:32 -0700

    W

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -8,8 +9,8 @@
 
 int main(void)
 {
-	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\r";
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(s, 1, sizeof(s), stderr);
+	write(2, s, sizeof(s));
 	return (1);
 }
