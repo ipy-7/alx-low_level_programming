3c9647e7f6d23bc88bfda590aac4dc2d949386f4
ipy-7 <ipy-7@github.com>
2023-06-16 13:18:02 +0200

    W

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,11 +3,12 @@
 #include <time.h>
 
 /**
- * main: Entry point
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  *
  */
+
 int main(void)
 {
 	int n;
@@ -15,9 +16,12 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n < 0) printf("%d is negative\n", n);
-	else if (n > 0) printf("%d is positive\n", n);
-	else printf("%d is zero\n", n);
+	if (n < 0)
+		printf("%d is negative\n", n);
+	else if (n > 0)
+		printf("%d is positive\n", n);
+	else
+		printf("%d is zero\n", n);
 
 	return (0);
 }
