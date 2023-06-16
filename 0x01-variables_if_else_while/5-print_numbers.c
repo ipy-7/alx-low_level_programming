20f73a4c24ba29c0e5ed34d12ff2ded16b2c74d4
ipy-7 <ipy-7@github.com>
2023-06-16 13:47:16 +0200

    W

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
