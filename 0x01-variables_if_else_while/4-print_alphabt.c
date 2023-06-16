761081321b2bd9d96df61e492def0b560c2da950
ipy-7 <ipy-7@github.com>
2023-06-16 13:44:42 +0200

    W

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
