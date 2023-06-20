474ca77208683a053d42ff1c206cbb733c5734bf
ipy-7 <ipy-7@github.com>
2023-06-20 23:39:15 +0200

    W

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - wwwwwwwwwww
+ * @n: Wwwwwww
+ *
+ * Description: WWWWWWWWWWW
+ *
+ * Return - 1 if greater than zero, -1 if less than zero, otherwise 0
+int print_sign(int n)
+{
+	int ret;
+
+	if (n > 0)
+	{
+		putchar('+');
+		ret = 1;
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		ret = -1;
+	}
+	else
+	{
+		putchar('0');
+		ret = 0;
+	}
+
+	return (ret);
+}
