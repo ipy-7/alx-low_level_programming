cfbf9e15740ddb085ba85afa2e9792fdcc49f6ff
ipy-7 <ipy-7@github.com>
2023-06-21 00:05:43 +0200

    W

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - wtwwwwwww
+ * @c: the character to check
+ *
+ * Description: WWWWWWWWWWW
+ *
+ * Return: 1 if the character is in lowercase, otherwise 0
+ */
+int print_last_digit(int c)
+{
+	printf("%d", c % 10);
+	return (c % 10);
+}
