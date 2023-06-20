89d8784168ee8d5a3e24dfc1a7a5d2b57ba26e23
ipy-7 <ipy-7@github.com>
2023-06-20 23:21:42 +0200

    W

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - wtwwwwwww
+ * @c: the character to check
+ *
+ * Description: WWWWWWWWWWW
+ *
+ * Return: 1 if the character is in lowercase, otherwise 0
+ */
+int _islower(int c)
+{
+	return islower(c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,3 +8,4 @@
 void print_alphabet(void);
 void _putchar(char c);
 void print_alphabet_x10(void);
+int _islower(int c);
