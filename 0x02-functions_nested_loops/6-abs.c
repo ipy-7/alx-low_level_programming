ddb57b32becd3a6d38ce43da7a6f196a407334c4
ipy-7 <ipy-7@github.com>
2023-06-21 00:01:57 +0200

    W

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - wtwwwwwww
+ * @c: the character to check
+ *
+ * Description: WWWWWWWWWWW
+ *
+ * Return: 1 if the character is in lowercase, otherwise 0
+ */
+int _abs(int c)
+{
+	return (c > 0 ? c : -c);
+}
