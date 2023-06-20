c5a29d7d654496e432d8d14c7d00b8c1f4a4d1e8
ipy-7 <ipy-7@github.com>
2023-06-21 00:32:52 +0200

    W

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - wtwwwwwww
+ * @a: the character to check
+ * @b: WWWWWWWWW
+ *
+ * Description: WWWWWWWWWWW
+ *
+ * Return: 1 if the character is in lowercase, otherwise 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
