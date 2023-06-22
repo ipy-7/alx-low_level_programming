8bae90007211211b29fb3b6b004c423cdb59b047
ipy-7 <ipy-7@github.com>
2023-06-22 20:09:10 +0200

    W

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * more_numbers - wwwwwwwwww
+ *
+ * Description: WWWWWWWWWWW
+ */
+void more_numbers(void)
+{
+	int i, k;
+
+	for (k = 0; k < 9; k++)
+	{
+		int flag;
+
+		flag = 0;
+		for (i = 48; i <= 57; i++)
+		{
+			if (!flag)
+				_putchar('1');
+			_putchar(i);
+
+			if (i == 57)
+			{
+				i = 48;
+				flag = 1;
+			}
+
+			if (flag && i == 4)
+				break;
+		}
+
+		_putchar('\n');
+	}
+}
