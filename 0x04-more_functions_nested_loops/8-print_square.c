a051d1fef40f4213bdebca7bbd7286b86bf99261
ipy-7 <ipy-7@github.com>
2023-06-22 21:15:56 +0200

    W

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_square - wwwwww
+ * @size: WWWWWWWWW
+ *
+ * Description: WWWWWWWWW
+ */
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+}
