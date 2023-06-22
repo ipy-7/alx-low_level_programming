592e1921de66bbe0e2fc5a149eafa94c9e418626
ipy-7 <ipy-7@github.com>
2023-06-22 20:26:20 +0200

    W

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal - wwwwww
+ * @n: WWWWWWWWW
+ *
+ * Description: WWWWWWWWW
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		_putchar(' ');
+	}
+	_putchar('\\');
+	_putchar('\n');
+}
