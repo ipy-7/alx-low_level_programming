ff161c1becca14f1b68d6277d31cc0515a5b42bb
ipy-7 <ipy-7@github.com>
2023-06-22 19:11:51 +0200

    W

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - wwwwwwwwww
+ *
+ * Description: WWWWWWWWWWW
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
