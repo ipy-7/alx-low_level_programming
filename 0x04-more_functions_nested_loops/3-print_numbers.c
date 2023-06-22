2e42368447341d92c80dbda29e42a6cd6ba24d38
ipy-7 <ipy-7@github.com>
2023-06-22 19:09:34 +0200

    W

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - wwwwwwwwww
+ *
+ * Description: WWWWWWWWWWW
+ */
+int print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
