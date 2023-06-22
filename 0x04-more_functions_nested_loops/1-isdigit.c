819c764427905b983cc20361de181b3451048d72
ipy-7 <ipy-7@github.com>
2023-06-22 19:04:20 +0200

    W

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - wtwwwwwww
+ * @c: the character to check
+ *
+ * Description: WWWWWWWWWWW
+ *
+ * Return: 1 if the character is in uppercase, otherwise 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -8,6 +8,11 @@
  */
 int main(void)
 {
-	printf("%d\n", _isupper('W'));
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
 	return (0);
 }
