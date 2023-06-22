53e65e2e5c64e4db8e4bca8674678e313a11ba74
ipy-7 <ipy-7@github.com>
2023-06-22 20:21:58 +0200

    W

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_line - wwwwwwwwww
+ *
+ * Description: WWWWWWWWW
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
