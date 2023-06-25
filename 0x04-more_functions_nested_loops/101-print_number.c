22db1353a321c44e7aab76bfdd43070568a02a21
ipy-7 <ipy-7@github.com>
2023-06-25 20:51:08 +0200

    W

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,6 +10,14 @@
  */
 void print_number(int n)
 {
-	if (n >= 10) print_number(n / 10);
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n >= 10)
+		print_number(n / 10);
+
 	_putchar(48 + (n % 10));
 }
--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	print_number(402);
 	_putchar('\n');
-	print_number(1024);
+	print_number(-1024);
 	_putchar('\n');
 
 	return (0);
