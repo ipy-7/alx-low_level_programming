a57025e9a8922730339a3beb8bff1e44dd3fb7c1
ipy-7 <ipy-7@github.com>
2023-06-26 18:22:05 +0200

    W

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,10 +10,15 @@
  */
 void puts_half(char *str)
 {
-	int i, len;
+	int i, len, start;
 
 	len = strlen(str);
-	for (i = (len - 1) / 2; i < len; i++)
+	if (len & 1)
+		start = (len - 1) / 2;
+	else
+		start = len / 2;
+
+	for (i = start; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
