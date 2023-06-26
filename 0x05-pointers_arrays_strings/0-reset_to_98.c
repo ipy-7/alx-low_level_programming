4cf1f1ab4c966d20106a19e12069261d4e4c19d2
ipy-7 <ipy-7@github.com>
2023-06-26 17:32:47 +0200

    W

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - WWWWWWW
+ * @n: WWWWWWWWWW
+ *
+ * Description: WWWWWWWWW
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void _putchar(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void fizz_buzz(void);
+void print_triangle(int size);
+int isPrime(long n);
+long largestPrimeFactor(long n);
+void print_number(int n);
+void reset_to_98(int *n);
+#endif /* !MAIN_H */
