ea3e9cf9103de10a1554e6da630ff529a666e18d
ipy-7 <ipy-7@github.com>
2023-07-03 18:39:12 +0200

    W

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_diagsums - WWWWWWW
+ * @a: WWWWWWWWWW
+ * @size: wWWWWWWWWWW
+ * Description: WWWWWWWWW
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, d1, d2;
+
+	d1 = 0;
+	d2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		d1 += a[i][i];
+		d2 += a[i][size - i - 1];
+	}
+
+	printf("%d, %d", d1, d2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -31,4 +31,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif /* !MAIN_H */
