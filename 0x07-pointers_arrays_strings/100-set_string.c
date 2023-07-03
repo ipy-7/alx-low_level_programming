3db5ace57e679b008381d58929b78ddb7ec24a3a
ipy-7 <ipy-7@github.com>
2023-07-03 19:57:27 +0200

    W

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * set_string - WWWWWWW
+ * @s: WWWWWWWWWW
+ * @to: WWWWWWWWWWW
+ * Description: WWWWWWWWW
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -32,4 +32,5 @@ char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 #endif /* !MAIN_H */
