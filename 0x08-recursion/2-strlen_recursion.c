4234ef8ae46c39aaf84d31920efd98f509c447be
ipy-7 <ipy-7@github.com>
2023-07-05 23:04:35 +0200

    W

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - WWWWWWWWW
+ * @s: WWWWWWWWWW
+ * Description: WWWWWWWWWW
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(s + 1));
+}
