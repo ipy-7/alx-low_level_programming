bcc0f5528f362e7726c233725e60673818d5a3f8
ipy-7 <ipy-7@github.com>
2023-07-05 23:12:15 +0200

    W

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - WWWWWWWWW
+ * @x: WWWWWWWWWW
+ * @y: WWWWWWWWWW
+ * Description: WWWWWWWWWW
+ * Return: djlakjwdldj
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
+}
