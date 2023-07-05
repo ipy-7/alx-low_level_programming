92de8e940349dbeeb050b50565add52936ab7e9d
ipy-7 <ipy-7@github.com>
2023-07-06 01:01:39 +0200

    W

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,10 +4,11 @@
 /**
  * is_prime_number - WWWWWWWWW
  * @n: WWWWWWWWWW
+ * @i: WWWWWW
  * Description: WWWWWWWWWW
  * Return: djlakjwdldj
  */
-int is_prime_number(int n, int i = 0)
+int is_prime_number(int n, int i)
 {
 	if (n <= 0)
 		return (0);
@@ -19,4 +20,15 @@ int is_prime_number(int n, int i = 0)
 		return (0);
 
 	return (is_prime_number(n, i + 1));
+}
+
+/**
+ * is_prime_number - WWWWWWWWWW
+ * @n: WWWWWWWWWW
+ * Description: WWWWWWWWWW
+ * Return: WWWWWWW
+ */
+int is_prime_number(int n)
+{
+	return (is_prime_number(n, 0));
 }
