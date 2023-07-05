517bf4e910918a126b15ac83d5a83d6105f85f11
ipy-7 <ipy-7@github.com>
2023-07-06 01:27:55 +0200

    W

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+ * _sqrt - WWWWWWWWW
+ * @n: WWWWWWWW
+ * @i: WWWWWWWWWW
+ * Description: WWWWWWWW
+ * Return: WWWWWWW
+ */
+int _sqrt(int n, int i)
+{
+	if (n <= 0 || (i * i) > n)
+		return (-1);
+
+	if ((i * i) == n)
+		return (i);
+
+	return (_sqrt(n, i + 1));
+}
+
+/**
  * _sqrt_recursion - WWWWWWWWW
  * @n: WWWWWWWWWW
  * Description: WWWWWWWWWW
@@ -9,5 +27,5 @@
  */
 int _sqrt_recursion(int n)
 {
-
+	return (_sqrt(n, 1));
 }
