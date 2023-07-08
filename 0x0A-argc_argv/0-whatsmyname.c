4b3244d1ee802821226e3549447a260fa2b2825d
ipy-7 <ipy-7@github.com>
2023-07-09 01:24:53 +0200

    W

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- *
+ * @argc: command line argument count
+ * @argv: an array of strings storing command line arguments of size argc
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
