8b022c57a8cc27f2348344ffdb98232c1a2fdec7
ipy-7 <ipy-7@github.com>
2023-07-10 21:21:33 +0200

    W

---
+++ 0x0B-malloc_free/4-gree_grid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * free_grid - WWWWWWWWWWW
+ * @grid: WWWWWWWWW
+ * @h: WWWWWWWWWW
+ * Description: WWWWWWWWWW
+ */
+void alloc_grid(int **grid, int h)
+{
+	int i;
+
+	for (i = 0; i < h; i++)
+	{
+		free(grid[i]);
+	}
+}
