f34972cc572225b4b7677a74b4ff8a49bb468f3b
ipy-7 <ipy-7@github.com>
2023-07-10 19:44:35 +0200

    W

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strdup - WWWWWWWWWWW
+ * @s1: WWWWWWWWW
+ * @s2: WWWWWWWWWW
+ * Description: WWWWWWWWWW
+ * Return: A pointer to the array
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i;
+	char *ptr;
+
+	if (s1 == NULL)
+	{
+		len1 = 0;
+	}
+	else
+	{
+		len1 = strlen(s1);
+	}
+
+	if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+	else
+	{
+		len2 = strlen(s2);
+	}
+
+	ptr = (char *)malloc(len1 + len2);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len1; i++)
+	{
+		ptr[i] = s1[i];
+	}
+
+	for (i = 0; i < len2; i++)
+	{
+		ptr[i] = s2[i];
+	}
+
+	return (ptr);
+}
