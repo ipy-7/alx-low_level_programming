2dc1ccecc913efe17275f4611a5aa6fc932c716c
ipy-7 <ipy-7@github.com>
2023-07-10 20:59:37 +0200

    W

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * alloc_grid - WWWWWWWWWWW
+ * @w: WWWWWWWWW
+ * @h: WWWWWWWWWW
+ * Description: WWWWWWWWWW
+ * Return: A pointer to the array
+ */
+int **alloc_grid(int w, int h)
+{
+	int i, j;
+	int **grid;
+
+	if (w <= 0 || h <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = (int**)malloc(h);
+	
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < h; i++)
+	{
+		grid[i] = (int*)malloc(sizeof(int) * w);
+		if (grid[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < h; i++)
+	{
+		for (j = 0; j < w; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+
+	return (grid);
+}
