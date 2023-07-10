d86ca3b3f56366a604f91eb3bbd6da21980bbbf7
ipy-7 <ipy-7@github.com>
2023-07-10 19:10:01 +0200

    W

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * create_array - WWWWWWWWWWW
+ * @size: WWWWWWWWW
+ * @c: WWWWWWWWWWW
+ * Description: WWWWWWWWWW
+ * Return: A pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *ptr;
+
+	ptr = (char *)malloc(size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
+}
