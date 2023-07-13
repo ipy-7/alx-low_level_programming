e0882d507767b6b4a152663c32eae9d27c1a8a92
ipy-7 <ipy-7@github.com>
2023-07-13 20:01:39 +0200

    W

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - WWWWWWWWW
+ * @b: WWWWWWWWWW
+ * Description: WWWWWWWWWWW
+ * Return: pointer to the allocated memory
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,41 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+void _putchar(int c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void fizz_buzz(void);
+void print_triangle(int size);
+int isPrime(long n);
+long largestPrimeFactor(long n);
+void print_number(int n);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
+void _puts_recursion(char *s);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char **strtow(char *s);
+#endif /* !MAIN_H */
