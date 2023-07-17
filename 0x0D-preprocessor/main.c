aaad51d7d54b89d1afacbe1a27ceeda1a047a669
ipy-7 <ipy-7@github.com>
2023-07-17 18:32:09 +0200

    W

---
+++ 0x0D-preprocessor/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+
+/**
+ * main - Entry point
+ * Description: WWWWWWWWWW
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%d\n", ABS(-1));
+
+	return (0);
+}
