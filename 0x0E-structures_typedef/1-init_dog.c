ec620ace2668ec7658d0c3f5cb1407c5136abc8a
ipy-7 <ipy-7@github.com>
2023-07-18 00:23:55 +0200

    W

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - WWWWWWWWWW
+ * @d: WWWWWWWWW
+ * @name: WWWWWWWWW
+ * @age: WWWWWWWWWWW
+ * @owner: WWWWWWWWWW
+ * Description: WWWWWWWWWWWW
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
