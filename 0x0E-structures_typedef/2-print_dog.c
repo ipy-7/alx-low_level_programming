5d1651b64466528a51654a8244f8135a0551f653
ipy-7 <ipy-7@github.com>
2023-07-18 00:46:41 +0200

    W

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,9 +12,6 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: %s\n", (d->name != NULL ? d->name : "(nil)"));
-	if (d->age == 0.0)
-		printf("Age: %s\n", "(nil)");
-	else
-		printf("Age: %f\n", d->age);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", (d->owner != NULL ? d->owner : "(nil)"));
 }
