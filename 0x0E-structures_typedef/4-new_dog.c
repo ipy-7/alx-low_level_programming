13cb0c676d3e72f976e6564ffc787d5609e5efe6
ipy-7 <ipy-7@github.com>
2023-07-18 01:12:08 +0200

    W

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,20 +21,31 @@ dog_t *new_dog(char *name, float age, char *owner)
 	nameCpy = malloc(strlen(name) + 1);
 	ownerCpy = malloc(strlen(owner) + 1);
 
-	if (nameCpy == NULL || ownerCpy == NULL || d == NULL)
+	if (d == NULL)
 		return (NULL);
 
-	for (i = 0; i < strlen(name); i++)
+	if (nameCpy == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+
+	if (ownerCpy == NULL)
+	{
+		free(d);
+		free(nameCpy);
+		return (NULL);
+	}
+
+	for (i = 0; i <= strlen(name); i++)
 	{
 		nameCpy[i] = name[i];
 	}
-	nameCpy[i] = '\0';
 
-	for (i = 0; i < strlen(owner); i++)
+	for (i = 0; i <= strlen(owner); i++)
 	{
 		ownerCpy[i] = owner[i];
 	}
-	ownerCpy[i] = '\0';
 
 	d->name = nameCpy;
 	d->age = age;
