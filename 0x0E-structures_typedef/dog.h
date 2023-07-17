6fb1e5de1f1095e85ea3c2a0046d79460e351dcc
ipy-7 <ipy-7@github.com>
2023-07-18 00:21:01 +0200

    W

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - WWWWWWWWWWW
+ * @name: WWWWWWWWW
+ * @age: WWWWWWWWWW
+ * @owner: WWWWWWWWWWW
+ *
+ * Description: WWWWWWWW
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
