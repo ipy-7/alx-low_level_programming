d48571d9e5bf4cafd0189be9f9323d7c9e1cb32a
ipy-7 <ipy-7@github.com>
2023-07-19 18:52:16 +0200

    W

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_name - WWWWWWWWW
+ * @name: WWWWWWWW
+ * @f: WWWWWWWWWW
+ * Description: WWWWWWWW
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
