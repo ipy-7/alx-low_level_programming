9810cdededea23d8a56b27a87f2f8b1eb02820e8
ipy-7 <ipy-7@github.com>
2023-07-19 19:08:01 +0200

    W

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,4 +3,5 @@
 #include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, size_t size, void (*cmp)(int));
 #endif
