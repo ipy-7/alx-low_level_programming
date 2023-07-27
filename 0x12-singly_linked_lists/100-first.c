5745a3337297709fde53876eb1b40f4950e4ff92
ipy-7 <ipy-7@github.com>
2023-07-28 00:30:29 +0200

    W

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * first - WWWWWWWW
+ * @head: WWWWWWWWWWWWW
+ * Return: WWWWWWW
+ */
+void first()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
