ba8063c14b405bd8183c1de6e6ea98c4182e356c
ipy-7 <ipy-7@github.com>
2023-07-28 00:06:50 +0200

    W

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,13 +10,16 @@
 size_t print_list(const list_t *h)
 {
 	size_t i;
-	list_t *curr;
+	const list_t *curr;
 
 	curr = h;
 	i = 0;
 	while (curr)
 	{
-		printf("[%d] %s\n", curr->len, curr->str ? curr->str : "(nil)");
+		if (curr->str)
+			printf("[%d] %s\n", curr->len, curr->str);
+		else
+			printf("[0] (nil)\n");
 		curr = curr->next;
 		i++;
 	}
