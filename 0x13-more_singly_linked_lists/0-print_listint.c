958abee04cc54f451196c8eb7ca96e7d2d72bbb0
ipy-7 <ipy-7@github.com>
2023-08-01 09:15:23 +0200

    W

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - WWWWWWWW
+ * @h: WWWWWWWWWW
+ * Return: WWWWWWWW
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t cnt;
+	const listint_t *curr;
+
+	cnt = 0;
+	curr = h;
+	while (curr)
+	{
+		printf("%d\n", curr->n);
+		cnt++;
+	}
+
+	return (cnt);
+}
