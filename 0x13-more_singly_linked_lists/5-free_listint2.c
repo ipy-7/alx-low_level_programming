fad8633ed6b226f56192a8e63af8c00d2a1ff467
ipy-7 <ipy-7@github.com>
2023-08-01 10:18:30 +0200

    W

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,12 +11,13 @@ void free_listint2(listint_t **h)
 {
 	listint_t *tmp;
 
-	while (*h)
+	while (**h && *h)
 	{
 		tmp = (*h)->next;
 		free(*h);
 		(*h) = tmp;
 	}
 
-	(*h) = NULL;
+	if (**h)
+		(*h) = NULL;
 }
