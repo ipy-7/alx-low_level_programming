9aab6d8f9d566c4bcad7a01d38efd347be21594b
ipy-7 <ipy-7@github.com>
2023-08-04 13:50:40 +0200

    W

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * power - WWWWWWWWWW
+ * @x: WWWWWWWW
+ * @y: WWWWWWWW
+ * Return: WWWWWW
+ */
+int power(unsigned int x, unsigned int y)
+{
+	return (y == 0 ? 1 : x * power(x, y - 1));
+}
+
+/**
+ * print_binary - WWWWWWWW
+ * @n: WWWWWWWW
+ * Return: WWWWWWWWW
+ */
+void print_binary(unsigned long int n)
+{
+	while (n)
+	{
+		printf("%d\n", n & 1);
+		n >>= 1;
+	}
+}
