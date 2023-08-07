00eb50c090528176f8a61e85cae203e382b6378c
ipy-7 <ipy-7@github.com>
2023-08-07 14:43:28 +0200

    W

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - WWWWWWWWWW
+ * @filename: WWWWWWWWWWW
+ * @text_content: WWWWWWW
+ * Return: WWWWWWWWW
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, written;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		written = write(fd, text_content, strlen(text_content));
+		return (written == (int)strlen(text_content) ? 1 : -1);
+	}
+
+	return (1);
+}
