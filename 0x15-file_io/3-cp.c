fb59d71706b19dac32b4909cd715ab48c5d3bf26
ipy-7 <ipy-7@github.com>
2023-08-07 15:54:10 +0200

    W

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,6 +6,28 @@
 #include "main.h"
 
 /**
+ * handle_error - WWWWWWWW
+ * @fd_from: WWWWWWW
+ * @fd_to: WWWWWWW
+ * @av: WWWWW
+ * Return: WWWWWW
+ */
+void handle_error(int fd_from, int fd_to, char **av)
+{
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can\'t read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can\'t write to %s\n", av[2]);
+		exit(99);
+	}
+}
+
+/**
  * handle_close - WWWWWWWW
  * @fd_from: WWWWW
  * @fd_to: WWWWWW
@@ -15,12 +37,12 @@ void handle_close(int fd_from, int fd_to)
 {
 	if (close(fd_from) == -1)
 	{
-		dprintf(2, "Error: Can\'t close fd %d\n", fd_from);
+		dprintf(STDERR_FILENO, "Error: Can\'t close fd %d\n", fd_from);
 		exit(100);
 	}
 	if (close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can\'t close fd %d\n", fd_to);
+		dprintf(STDERR_FILENO, "Error: Can\'t close fd %d\n", fd_to);
 		exit(100);
 	}
 }
@@ -46,28 +68,20 @@ int main(int ac, char **av)
 	fd_from = open(file_from, O_RDONLY);
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC | O_APPEND, 0664);
 
-fd_from_fail:
-	dprintf(2, "Error: Can\'t read from file %s\n", file_from);
-	exit(98);
+	handle_error(fd_from, fd_to, av);
 
-fd_to_fail:
-	dprintf(2, "Error: Can\'t write to %s\n", file_to);
-	exit(99);
-
-	if (fd_from == -1)
-		goto fd_from_fail;
-	if (fd_to == -1)
-		goto fd_to_fail;
 	r_val = 1;
 	while (r_val != 0)
 	{
 		r_val = read(fd_from, buffer, 1024);
+
 		if (r_val == -1)
-			goto fd_from_fail;
-		buffer[r_val] = '\0';
+			handle_error(-1, fd_to, av);
+
 		wr_val = write(fd_to, buffer, r_val);
+
 		if (wr_val == -1)
-			goto fd_to_fail;
+			handle_error(fd_from, -1, av);
 	}
 	handle_close(fd_from, fd_to);
 	return (0);
