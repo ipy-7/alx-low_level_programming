b5e051a5c4004e022e3545940c76e9aa761a5257
ipy-7 <ipy-7@github.com>
2023-08-07 14:53:15 +0200

    W

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0x600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0x600);
 	if (fd == -1)
 		return (-1);
 
@@ -26,7 +26,11 @@ int create_file(const char *filename, char *text_content)
 	{
 		written = write(fd, text_content, strlen(text_content));
 		close(fd);
-		return (written == (int)strlen(text_content) ? 1 : -1);
+
+		if (written == -1)
+			return (-1);
+	
+		return (1);
 	}
 
 	close(fd);
